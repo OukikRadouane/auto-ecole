aba3595c0e11921b03f428cf0702e74654bfdd8f
OukikRadouane <oukikradouane@gmail.com>
2026-08-13 17:45:45 +0100

    configuration du routage API Gateway

---
+++ api-gateway/src/main/java/com/auto/apigateway/Config/GatewayRoutesConfig.java
@@ -0,0 +1,47 @@
+package com.auto.apigateway.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> authServiceRoute(){
+        return route("auth-service-route")
+                .route(path("/api/auth/**"), http())
+                .filter(lb("AUTH-SERVICE"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoute(){
+        return route("user-service-route")
+                .route(path("/api/users/**"), http())
+                .filter(lb("AUTH-SERVICE"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> seriesServiceRoute() {
+        return route("series-service-route")
+                .route(path("/api/series/**"), http())
+                .filter(lb("SERIES-SERVICE"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> examServiceRoute() {
+        return route("exam-service-route")
+                .route(path("/api/exam/**"), http())
+                .filter(lb("SERIES-SERVICE"))
+                .build();
+    }
+}
